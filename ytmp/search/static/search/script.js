bc34bdb69932e45ead4a96612b65d6c9ddc0c974
efmarcial <efmarcial1060@gmail.com>
2021-10-09 20:05:04 -0400

    didnt work

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -12,5 +12,5 @@ document.getElementById("demo").onclick = function() {helloWorld()};
 function helloWorld() {
 
     document.getElementById("demo").innerHTML = "You CLicked Me";
-    document.getElementById("demo").console.log("You pressed download button")
+    console.log("Downloading..........");
 };
