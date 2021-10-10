aee4721ffbe38369488c823406b7da0460d20d88
efmarcial <efmarcial1060@gmail.com>
2021-10-10 19:05:28 -0400

    l

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -7,19 +7,6 @@
     };
 
    
-    const myForm = document.querySelector("form1");
-
-    myForm.addEventListener("myButton", function(event){
-        console.log("Clicked a button......");
-        console.log("Downloading......page");
-
-        var demo =document.getElementById('demo');
-        demo.onclick = function(){videoURL()};
-        function videoURL(){
-            console.log("Clicked");
-            };
-
-    });
 
     
 
