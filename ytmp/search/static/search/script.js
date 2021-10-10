8af1d292c2da4c114bc4c0aa8a48645eddf4fbf9
efmarcial <efmarcial1060@gmail.com>
2021-10-10 00:03:24 -0400

    for loop

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -3,7 +3,7 @@ function load(){
 
     for(var counter =0; counter > 10; counter++){
 
-        document.getElementById('divBtn').onclick = function() {videoURL()};
+        document.getElementById('demo').onclick = function() {videoURL()};
         function videoURL() {
             var videourl = document.getElementById('demo').getAttribute('title');
 
