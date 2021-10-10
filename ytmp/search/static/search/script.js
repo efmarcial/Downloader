e773dce3686a14f902cf136d9010ee6dc0cec63e
efmarcial <efmarcial1060@gmail.com>
2021-10-09 21:54:37 -0400

    I am able to get video url by using the ID for the button and href from the view <a>. When I press the download button it print the url in the console log of the browser

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,16 +1,12 @@
 
 window.onload = function() {
-    function helloWorld(){
-        console.log('Hello world from Java')
-    };
     
-    helloWorld();
-    var videourl = document.getElementById('video').getAttribute('href');
-    document.getElementById("demo").onclick = function() {HelloWorld()};
-    function HelloWorld() {
+    document.getElementById("demo").onclick = function() {videoURL()};
+    function videoURL() {
+        
+        var videourl = document.getElementById('video').getAttribute('href');
 
         console.log("Downloading..........");
-        
         console.log(videourl);
     };
 
