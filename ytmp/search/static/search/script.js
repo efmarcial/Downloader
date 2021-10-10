836ab2bd7cfe3ce7b189a03b31e034d31a543e60
efmarcial <efmarcial1060@gmail.com>
2021-10-09 22:48:23 -0400

    commit

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -2,7 +2,6 @@
 
     document.getElementById("demo").onclick = function() {videoURL()};
     function videoURL() {
-        var id = document.getElementsId("demo");
         var videourl = document.getElementsByTagName(a).getAttribute('title');
 
         console.log("Downloading..........");
