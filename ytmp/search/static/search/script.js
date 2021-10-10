4bfcf6910672fd234498035a7b754b95d9944cd4
efmarcial <efmarcial1060@gmail.com>
2021-10-09 20:02:07 -0400

    just messing around

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -6,3 +6,11 @@ function helloWorld(){
 
 helloWorld();
 
+document.getElementById("demo").onclick = function() {helloWorld()};
+
+
+function helloWorld() {
+
+    document.getElementById("demo").innerHTML = "You CLicked Me";
+    document.getElementById("demo").console.log("You pressed download button")
+};
