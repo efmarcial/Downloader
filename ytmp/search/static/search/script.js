4455f9a47dfd191f197afdd9513c1556edc81513
efmarcial <efmarcial1060@gmail.com>
2021-10-09 20:18:18 -0400

    missing 'window.onload function()' to execute the script after the DOM is ready

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,7 +1,12 @@
 
+window.onload = function() {
+    function helloWorld(){
+        console.log('Hello world from Java')
+    };
+    
+    helloWorld();
 
-
-document.getElementById("demo").onclick = function() {HelloWorld()};
+    document.getElementById("demo").onclick = function() {HelloWorld()};
 
 
 function HelloWorld() {
@@ -9,3 +14,5 @@ function HelloWorld() {
     document.getElementById("demo").innerHTML = "You CLicked Me";
     console.log("Downloading..........");
 };
+    
+}
