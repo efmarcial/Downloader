dd2464e1367fe26a66f59df0c10696fd3d7321b8
efmarcial <efmarcial1060@gmail.com>
2021-10-10 17:04:25 -0400

    idk

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,10 +1,19 @@
 
 
-    document.getElementById('demo').onclick = function(){videoURL()};
+    var demo =document.getElementById('demo');
+    demo.onclick = function(){videoURL()};
     function videoURL(){
         console.log("Clicked");
     };
 
+    var mybutton = document.form1.myButton;
+    var numberClicked = 0;
+
+    function myButtonClicked() {
+        console.log("Clicked button")
+    };
+
+    mybutton.addEventListener("'click", myButtonClicked);
 
 
 
