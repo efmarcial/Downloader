9e381ffd832e5dfbce89bb30b7f6f51d1c57f253
efmarcial <efmarcial1060@gmail.com>
2021-10-10 00:16:41 -0400

    started from zero

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,16 +1,11 @@
 
 function load(){
 
-    for(var counter=0; counter > 10; counter++){
-        document.getElementById('demo')[counter].onclick = function() {videoURL()};
-        function videoURL() {
-            var videourl = document.getElementById('demo').getAttribute('title');
-
-            console.log("Downloading..........");
-            console.log(videourl);
-        
-        };
+    document.getElementById('demo').onclick = function(){videoURL};
+    function videoURL(){
+        console.log("Clicked");
     };
+
 };
 window.onload = load;
 
