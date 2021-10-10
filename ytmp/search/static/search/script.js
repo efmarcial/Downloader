010394d600c2fabc4c8a8a4157418d624c8f31dc
efmarcial <efmarcial1060@gmail.com>
2021-10-09 23:30:51 -0400

    trying sominthing

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,12 +1,13 @@
 
 
-    document.getElementById('demo').onclick = function() {videoURL()};
-    function videoURL() {
-        var id = document.getElementById("demo");
-        var videourl = document.getElementById('demo').getAttribute('title');
 
-        console.log("Downloading..........");
-        console.log(videourl);
-    };
 
+document.getElementById('demo').getAttribute('title').onclick = function() {VideoURLS()};
+
+function VideoURLS(){
     
+    var urls = document.getElementById('demo').getAttribute('title');
+    console.log("Converting.......");
+    console.log(urls)
+};
+
