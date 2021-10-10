45077040ac177124324829324f9c51ce8814c115
efmarcial <efmarcial1060@gmail.com>
2021-10-10 19:29:12 -0400

    m

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -6,6 +6,17 @@
         console.log("Clicked");
     };
 
+        var div = document.getElementById("myDIV");
+        var nodelist = div.getElementsByClassName("btn btn-sm btn-outline-secondary");
+
+        var i;
+        for(i=0; i < nodelist.length; i++) {
+          console.log(nodelist[i]);
+
+        };
+
+    
+
    
 
     
