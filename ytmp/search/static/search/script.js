4a69773f17b87e73503db417e5c3a032cb393d1e
efmarcial <efmarcial1060@gmail.com>
2021-10-10 00:12:07 -0400

    blah

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,16 +1,16 @@
 
 function load(){
 
-
-        document.getElementById('demo').onclick = function() {videoURL()};
+    for(var counter=0; counter > 10; counter++){
+        document.getElementById('demo')[counter].onclick = function() {videoURL()};
         function videoURL() {
             var videourl = document.getElementById('demo').getAttribute('title');
 
             console.log("Downloading..........");
             console.log(videourl);
         
-
         };
+    };
 };
 window.onload = load;
 
