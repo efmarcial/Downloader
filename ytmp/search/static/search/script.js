c6af287effec2e72d8c41d4c244f11827598c863
efmarcial <efmarcial1060@gmail.com>
2021-10-10 00:21:34 -0400

    hello

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,12 +1,9 @@
 
-function load(){
 
     document.getElementById('demo').onclick = function(){videoURL};
     function videoURL(){
         console.log("Clicked");
     };
 
-};
-window.onload = load;
 
 
