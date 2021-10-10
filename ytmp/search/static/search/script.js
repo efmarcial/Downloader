383c610e99450fbc98357b945a457cbedbee5380
efmarcial <efmarcial1060@gmail.com>
2021-10-10 19:45:31 -0400

    this is it man

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -13,6 +13,13 @@
         for(i=0; i < nodelist.length; i++) {
           console.log(nodelist[i]);
 
+          var demo =document.getElementById('demo');
+          demo.onclick = function(){videoURL()};
+          function videoURL(){
+              console.log("Clicked");
+          };
+
+
         };
 
     
