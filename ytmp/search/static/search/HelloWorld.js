4ea2b8f1d2604008ea503fc411fce1565f486841
efmarcial <efmarcial1060@gmail.com>
2021-10-11 01:49:53 -0400

    debug

--- ytmp/search/static/search/HelloWorld.js
+++ ytmp/search/static/search/HelloWorld.js
@@ -5,6 +5,25 @@ var aElement = document.getElementsByTagName('a');
 var lenght = aElement.length;
 
 for (var i=0; i < lenght; i++){
-    console.log(aElement[i]);
+    //console.log(aElement[i]);
 }
 
+
+
+//Psuedo Code:
+//TODO: get all <a> tags and count them using lenght()
+const aElem = document.getElementsByTagName('a');
+var lenght = aElem.length;
+
+/*
+TODO: for loop all the 'a' tags to change the
+        id's of all the tags.
+*/
+
+for (i=0; i < lenght; i++){
+    aElem.id = 'demo'+i;
+}
+
+for(i=0; i < length; i++){
+    console.log(aElem[i]);
+}
