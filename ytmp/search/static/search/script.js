52fa02283acdbacd9d9c9e1489f17cab5d5efb81
efmarcial <efmarcial1060@gmail.com>
2021-10-10 20:33:18 -0400

    more debug

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -3,16 +3,20 @@
   
         var div = document.getElementById("myDIV");
         var nodelist = div.getElementsByClassName("btn btn-sm btn-outline-secondary");
-        var url = nodelist;
+    
         var i;
         for(i=0; i < nodelist.length; i++) {
           console.log(nodelist[i]);
 
           nodelist[i].onclick = function(){VideoURL()};
           function VideoURL(){
-              var vidurl = url.getAttribute("title");
+
+            var url = document.getElementById("demo").getAttribute("title");
+
+
               console.log("clicked");
-              console.log(vidurl);
+              console.log(url);
+
           };
 
         };
