5da187641f2d20a7ba69a30ce0f13869a0742cb5
efmarcial <efmarcial1060@gmail.com>
2021-10-11 01:52:25 -0400

    r

--- ytmp/search/static/search/HelloWorld.js
+++ ytmp/search/static/search/HelloWorld.js
@@ -1,12 +1,6 @@
 
 
 
-var aElement = document.getElementsByTagName('a');
-var lenght = aElement.length;
-
-for (var i=0; i < lenght; i++){
-    //console.log(aElement[i]);
-}
 
 
 
@@ -24,6 +18,12 @@ for (i=0; i < lenght; i++){
     aElem.id = 'demo'+i;
 }
 
+var aElement = document.getElementsByTagName('a');
+var lenght = aElement.length;
+
+for (var i=0; i < lenght; i++){
+    //console.log(aElement[i]);
+}
 for(i=0; i < length; i++){
-    console.log(aElem[i]);
+    console.log(aElement[i]);
 }
