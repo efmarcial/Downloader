78a4735f6fec71cea158b82079aa4e76f76c97e9
efmarcial <efmarcial1060@gmail.com>
2021-10-10 21:42:53 -0400

    d

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -8,7 +8,7 @@
         var i;
         for(i=0; i < nodelist.length; i++) {
           console.log(nodelist[i]);
-            var url = nodelist[i].getAttribute('title');
+            var url = nodelist.getAttribute('title');
 
           nodelist[i].onclick = function(){VideoURL()};
           function VideoURL(){
