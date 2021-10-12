36baef321d89c56240f9c49d7bb79c331297e68e
efmarcial <efmarcial1060@gmail.com>
2021-10-11 21:34:16 -0400

    id

--- ytmp/search/static/search/script.js
+++ ytmp/search/static/search/script.js
@@ -1,18 +1,19 @@
-
+        // get all the class "btn..." in the DOM
         const div = document.getElementById("myDIV");
         var nodelist = div.getElementsByClassName("btn btn-sm btn-outline-secondary");
         
         var urls = [];
-
+        // loop the lenght of all the classes "btn...." to get all <a>.
         var i;
         for(i=0; i < nodelist.length; i++) {
 
          // console.log(nodelist[i]);
-
+            // onClick Download btn to get url and pass it to js
           var tagId = document.getElementById("demo");
           nodelist[i].onclick = function(){VideoURL()};
           urls.push(nodelist[i][
-            'title'
+              'id',
+              'title'
           ]);
 
           function VideoURL(){
@@ -22,6 +23,7 @@
           };
           
         };
+        // Remove the empty " " index from the urls array
         urls = urls.filter(item => item);
         console.log(urls)
         
