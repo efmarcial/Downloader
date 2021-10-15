7f9902fc43c7d6caa479fa7b965d8091130b89b0
efmarcial <efmarcial1060@gmail.com>
2021-10-15 01:20:06 -0400

    g

--- ytmp/search/static/search/HelloWorld.js
+++ ytmp/search/static/search/HelloWorld.js
@@ -1,5 +1,7 @@
 
 
+const fs = require()
+
 
 function hello(url,name){
     var url = url;
@@ -7,3 +9,17 @@ function hello(url,name){
     console.log(name);
     console.log(url);
 };
+
+/*
+stream = ytdl(url)
+
+proc = new ffmpeg({source:stream})
+proc.setFfmpegPath('/Applications/ffmpeg')
+proc.saveToFile(mp3, (stdout, stderr)->
+            return console.log stderr if err?
+            return console.log 'done'
+        )
+
+
+
+*/
