50cfedca3e72509fa4dc8bac6901c764c76f2d8d
efmarcial <efmarcial1060@gmail.com>
2021-10-15 01:52:07 -0400

    trying to run node.js with django for heroku new buildpack. Hope this works

--- ytmp/search/static/search/HelloWorld.js
+++ ytmp/search/static/search/HelloWorld.js
@@ -1,14 +1,18 @@
 
 
-const fs = require()
-
+import ytdl from "ytdl-core";
+import ffmpeg from "ffmpeg";
+import { stderr } from "process";
 
 function hello(url,name){
     var url = url;
+    ytdl(url);
     var name = name;
     console.log(name);
     console.log(url);
 };
+
+
 
 /*
 stream = ytdl(url)
@@ -23,3 +27,14 @@ proc.saveToFile(mp3, (stdout, stderr)->
 
 
 */
+
+function convert(url){
+    var stream = ytdl(url);
+
+    var proc = new ffmpeg({source:stream});
+proc.setFfmpegPath('/Applications/ffmpeg');
+proc.saveToFile(mp3, (stdout, stderr)
+    );
+        
+
+}
