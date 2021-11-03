33eefe54d2fab07c7ba191d7cef6aa443869dd26
efmarcial <efmarcial1060@gmail.com>
2021-11-02 22:48:14 -0400

    n

--- ytmp/search/urls.py
+++ ytmp/search/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('',views.index, name='index' ),
     path('ajax/foo/', views.SomeFunction, name='ajax_foobar'),
-    path('/video/', views.youTube, name="video_url")
+    path('video/', views.youTube, name="video_url")
 ]
--- ytmp/search/views.py
+++ ytmp/search/views.py
@@ -3,6 +3,7 @@ from json.encoder import JSONEncoder
 from math import e
 from ntpath import join
 from ssl import CERT_NONE
+from django.urls.conf import path
 import requests 
 from re import search
 from django.shortcuts import render
@@ -123,7 +124,7 @@ def SomeFunction(request):
 
     tmpDir = os.listdir(destination)
     for item in tmpDir:
-        if item.endswith('mp3'):
+        if item.endswith('.mp3'):
             vid_path = destination + item
             print(item)
 
@@ -142,11 +143,107 @@ def youTube(request):
 
     if request.method == "POST":
 
-        video_url = request.POST
-        print("Good it worked")
-        status = "Good it worked"
-    else:
-            status = "didtn work"
+        video = request.POST
+        key = list(video.keys())
+        val = list(video.values())
+        postion = val.index('Download')
 
+        # video format and URL 
+
+        video_url = key[postion]
+        video_format = request.POST['formats']
+        print("Good it worked", video_format)
+        print('url is ', video_url)
+
+    else:
+
+        status = "didtn work"
+     
+        return HttpResponse(status)
+    
+
+    # Path to where the file its going to be downloaded 
+    download_path = PROJECT_ROOT + "/static/tmp/"
+ 
+    # Before a file is downloaded check if an mp3 or mp4 file 
+    # exist to be deleted for memory storage
+    tmpDir = os.listdir(download_path)
+    for item in tmpDir:
+        if item.endswith('.mp3') or item.endswith('.mp4'):
+            os.remove(os.path.join(download_path, item))
+    
+    # Check user input to run a specific function 
+
+    if video_format == 'mp4':
+         
+        file = download_mp4(url=video_url,path=download_path)
+
+        file_path =  file
+        # use this to return a mp4 file
+        return HttpResponse(file_path, headers={
+            'Content-Type' : 'application/vnd.mp4', 
+            'Content-Disposition': 'attachment; filename="video.mp4'
+        })
+
+    elif video_format == 'mp3':   
+
+        file = convert(url=video_url, path=download_path)
+
+        file_path = file
+        # use this to return a mp3 file
+        return HttpResponse(file_path, headers={
+            'Content-Type' : 'application/vnd.mp3',
+            'Content-Disposition' : 'attachment; filename = "audio.mp3"'
+            })
+
+def download_mp4(url, path):
+
+    yt = YouTube(url)
+
+    video =yt.streams.filter().first()
+        #print(video)
+
+
+    video_tite = yt.title
+    video_file=video.download(path)
+    print(video_file)
+
+    for item in os.listdir(path):
+        if item.endswith('.mp4'):
+            name = item
             
-    return HttpResponse(status)
+    mp4_path = path + item
+
+    print(mp4_path)
+      
+    return mp4_path
+
+def convert(url,path):
+
+
+    yt = YouTube(url)
+
+    # only_audio is still downloaded as mp4 need to change ext
+    video =yt.streams.filter(only_audio=True).first()
+        #print(video)
+
+    video_title = yt.title
+
+    # Download video file
+    video_file=video.download(path)
+    
+    # convert mp4 to mp3 file extention
+    
+    base, ext = os.path.splitext(video_file)
+    new_file = base + '.mp3'
+    os.rename(video_file, new_file)
+
+    for item in os.listdir(path):
+        if item.endswith(".mp3"):
+            name = item
+
+    mp3_path = path+name
+
+    print(mp3_path)
+
+    return mp3_path
