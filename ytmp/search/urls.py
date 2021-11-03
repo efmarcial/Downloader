b6c80b922b0ae01eebcda22cc98f0423909304d1
efmarcial <efmarcial1060@gmail.com>
2021-11-03 06:20:22 -0400

    b

--- ytmp/search/urls.py
+++ ytmp/search/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index, name='index' ),
-    path('ajax/foo/', views.SomeFunction, name='ajax_foobar'),
     path('video/', views.youTube, name="video_url")
 ]
