627313136649b00db9037004b0f0038d52e5bd97
Eduardo Marcial <efmarcial1060@gmail.com>
2021-11-29 20:28:37 -0500

    responsive image test on mobile with heroku

--- ytmp/search/views.py
+++ ytmp/search/views.py
@@ -5,9 +5,14 @@ from django.conf import settings
 from django.http import HttpResponse
 from .forms import ContactForm
 
+from .models import imggal
+
 
 def index(request):
-    form = ContactForm()
-    context = {'form': form}
-    return render(request, 'search/index.html')
+    
+    resultsdisplay = imggal.objects.all()
+    context = {
+        'imggal' : resultsdisplay
+    }
+    return render(request, 'search/index.html', context)
 
