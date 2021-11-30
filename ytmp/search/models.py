ced1ef7da97eab70fe5eeedb2bbdf0c542ab2bcf
Eduardo Marcial <efmarcial1060@gmail.com>
2021-11-30 01:08:58 -0500

    ADDED database

--- ytmp/search/models.py
+++ ytmp/search/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 
-class images(models.Model):
+class image(models.Model):
     tilte = models.CharField(max_length=100)
     desc = models.CharField(max_length=500)
     img = models.ImageField(upload_to = 'images/')
